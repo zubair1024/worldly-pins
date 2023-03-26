f592bc4eb3d4455ebecf374e9de7838bf698a5d4
Zubair Ahmed <zubair1024@gmail.com>
2023-03-26 13:31:45 +0200

    better loading screen

--- src/components/ScreenLoader.tsx
+++ src/components/ScreenLoader.tsx
@@ -2,9 +2,8 @@ import Image from 'next/image';
 
 const ScreenLoader = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-full">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Image src="/globe.svg" width={200} height={200} alt="globe_loader" />
-      <p className="pt-6 text-3xl font-light">Loading</p>
     </div>
   );
 };
